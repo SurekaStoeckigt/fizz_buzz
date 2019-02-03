1dfc4f037653026de02e02bc0793d96e62bd2bbc
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-02-03 18:58:23 +0000

    fizzbuzz test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,16 +2,16 @@
 require "fizzbuzz"
 describe "fizzbuzz" do 
     it 'returns "fizz" when passed 3' do
-        expect(fizzbuzz(3)).to eq 'fizz' # testing input against output expecting method to equal output fizz
+        expect(3.fizzbuzz).to eq 'fizz' # testing input against output expecting method to equal output fizz
     end 
     it 'returns "buzz" when passed 5' do
-        expect(fizzbuzz(5)).to eq 'buzz' # testing input against output expecting method to equal output fizz
+        expect(5.fizzbuzz).to eq 'buzz' # testing input against output expecting method to equal output fizz
     end 
     it 'returns "fizzbuzz" when passed 15' do
-        expect(fizzbuzz(15)).to eq 'fizzbuzz' # testing input against output expecting method to equal output fizz
+        expect(15.fizzbuzz).to eq 'fizzbuzz' # testing input against output expecting method to equal output fizz
     end
         it 'returns "4" when passed 4' do
-        expect(fizzbuzz(4)).to eq 4 # testing input against output expecting method to equal output fizz
+        expect(4.fizzbuzz).to eq 4 # testing input against output expecting method to equal output fizz
     end
 end
 
